7d92d3f7b992bbfcf45446a66087fa476bf88d25
wawerutimothy <waweru_timothy@yahoo.com>
2023-11-03 16:37:30 +0300

    edit items

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from item.models import Item
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -11,3 +11,4 @@ def index(request):
     return render(request, 'dashboard/index.html', {
         'items': items,
     })
+
--- item/forms.py
+++ item/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import Item
 input_classes = 'w-full py-4 px-6 rounded-xl-border'
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
@@ -10,6 +11,25 @@ class NewItemForm(forms.ModelForm):
             'category': forms.Select(attrs={
                 'class': input_classes
             }),
+            'name': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'description': forms.Textarea(attrs={
+                'class': input_classes
+            }),
+            'price': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'image': forms.FileInput(attrs={
+                'class': input_classes
+            }),
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
             'name': forms.TextInput(attrs={
                 'class': input_classes
             }),
