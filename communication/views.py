4fe37074dd8b9b8caa1d27dc367fb6e6029edcd6
wawerutimothy <waweru_timothy@yahoo.com>
2023-11-03 18:58:35 +0300

    view finalize

--- communication/views.py
+++ communication/views.py
@@ -20,4 +20,19 @@ def new_conversation(request, item_pk):
 
         if form.is_valid():
             conversation = Conversation.objects.create(item=item)
+            conversation.members.add(request.user)
+            conversation.members.add(item.created_by)
+            conversation.save()
+
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = conversation
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            return redirect('item:detail', pk=item_pk)
+    else:
+        form = ConversationMessageForm()
+    return render(request, 'conversation/new.html', {
+        'form': form
+    })
 
