8d422bfbcf534567eb152c0c9b545bac54bf6187
wawerutimothy <waweru_timothy@yahoo.com>
2023-11-08 11:49:15 +0300

    communication moddle

--- communication/views.py
+++ communication/views.py
@@ -15,7 +15,7 @@ def new_conversation(request, item_pk):
     conversations = Conversation.objects.filter(item=item).filter(members__in=[request.user.id])
 
     if conversations:
-        pass
+        return redirect('commuinication:detail', pk=conversations.first().id)
 
     if request.method == 'POST':
         form = ConversationMessageForm(request.POST)
@@ -44,4 +44,28 @@ def inbox(request):
 
     return render(request, 'communication/inbox.html', {
         'conversations': conversations
+    })
+
+@login_required
+def detail(request, pk):
+    conversation = Conversation.objects.filter(members__in=[request.user.id]).get(pk=pk)
+
+    if request.method == 'POST':
+        form = ConversationMessageForm(request.POST)
+
+        if form.is_valid():
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = conversation
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            conversation.save()
+
+            return redirect('communication:detail', pk=pk)
+    else:
+        form = ConversationMessageForm()
+
+    return render(request, 'communication/detail.html', {
+        'conversation': conversation,
+        'form': form
     })
